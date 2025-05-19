521186d99593fd00267a399445f4e7fce3c5fa9a
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-05-19 17:47:17 -0500

    push pop shift unshift

--- index.js
+++ index.js
@@ -15,3 +15,22 @@ let data = ["chris", 39, true, null, ["river", "tess", "pineapple", ["blueberry"
 console.log(data[4][3][2][2][0])
 
 console.log("should be zach: ", data[5])
+
+console.log("length of data: ", data.length)
+
+console.log("last element of data array: ", data[data.length - 1])
+
+let furniture = ["table", "chair", "sofa", "bed", "desk"];
+console.log("our furniture: ", furniture);
+
+furniture.push("lamp")
+console.log("our furniture after push: ", furniture);
+
+furniture.pop()
+console.log("our furniture after pop: ", furniture);
+
+furniture.unshift("shelf")
+console.log("our furniture after unshift: ", furniture);
+
+furniture.shift()
+console.log("our furniture after shift: ", furniture);
