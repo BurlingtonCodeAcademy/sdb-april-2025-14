6ce5e4c5e57b63882a0f9a1c9485eb19da1f2ea3
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-05-19 17:17:54 -0500

    first array

---
+++ index.js
@@ -0,0 +1,11 @@
+let fruitOne = "apple";
+let fruitTwo = "banana";
+let fruitThree = "cherry";
+
+let fruits = ["orange", "grape", "raspberry"];
+
+console.log(fruitOne);
+console.log(fruitTwo);
+console.log(fruitThree);
+
+console.log(fruits[0])
