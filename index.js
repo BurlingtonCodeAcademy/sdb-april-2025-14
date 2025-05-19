712068d37fa2641b2fba6fcd6a146f8de7839f3d
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-05-19 17:26:53 -0500

    navigating arrays

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ console.log(fruitTwo);
 console.log(fruitThree);
 
 console.log(fruits[0])
+
+let data = ["chris", 39, true, null, ["river", "tess", "pineapple", ["blueberry", "kiwi", ["cups", "plates", ["fish", "plants"]]]], "zach"];
+
+console.log(data[4][3][2][2][0])
+
+console.log("should be zach: ", data[5])
